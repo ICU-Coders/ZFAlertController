3c47cd8faf70570dfd619df621ff545868e9fb19
FranLucky <zhangfan8080@gmail.com>
2019-09-25 17:23:24 +0800

    readme

--- sample/ZFAlertViewController/ViewController.m
+++ sample/ZFAlertViewController/ViewController.m
@@ -19,21 +19,43 @@
     [super viewDidLoad];
     [self.view setBackgroundColor:[UIColor yellowColor]];
     
-    UIButton *button = [UIButton buttonWithType:UIButtonTypeContactAdd];
-    [button setFrame:CGRectMake(100, 100, 44, 44)];
-    [self.view addSubview:button];
-    [button addTarget:self action:@selector(buttonClicked:) forControlEvents:UIControlEventTouchUpInside];
 }
-
-- (void)buttonClicked:(UIButton *)button {
-    ZFAlertViewController *alertVC = [ZFAlertViewController alertWithTitle:@"标题？" message:@"副标题" style:ZFAlertViewControllerOptionMessage_Input];
+- (IBAction)buttclick:(UIButton *)sender {
     
+    int tag = (int)sender.tag;
+    ZFAlertViewControllerStyleOption option;
+    switch (tag) {
+        case 0:
+            option = ZFAlertViewControllerOptionTitle;
+            break;
+        case 1:
+            option = ZFAlertViewControllerOptionMessage;
+            break;
+        case 2:
+            option = ZFAlertViewControllerOptionInput;
+            break;
+        case 3:
+            option = ZFAlertViewControllerOptionTitle_Message;
+            break;
+        case 4:
+            option = ZFAlertViewControllerOptionTitle_Input;
+            break;
+        case 5:
+            option = ZFAlertViewControllerOptionTitle_Message_Input;
+            break;
+        default:
+            option = ZFAlertViewControllerOptionTitle_Message_Input;
+            break;
+    }
+    
+    ZFAlertViewController *alertVC = [ZFAlertViewController alertWithTitle:@"标题" message:@"描述" style:option];
     alertVC.textChangeCallback = ^(NSString * _Nonnull text, UITextField * _Nonnull textField) {
         NSLog(@"text:%@", text);
     };
     
     ZFAlertViewAction *ok = [ZFAlertViewAction actionWithTitle:@"确定" action:^{
         NSLog(@"ok");
+        [self testFunc];
     }];
     ok.titleColor = ZFALERT_OK_CLOLR;
     ZFAlertViewAction *cancel = [ZFAlertViewAction actionWithTitle:@"取消" action:^{
@@ -46,4 +68,8 @@
     [self presentViewController:alertVC animated:YES completion:nil];
 }
 
+
+- (void)testFunc {
+    NSLog(@"%s",  __func__);
+}
 @end
